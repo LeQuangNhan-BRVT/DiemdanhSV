bbf77135df1264f9039fc159c0e97a92cd2d6046
cowship <lequangnhanbrvt58@gmail.com>
2025-04-06 01:23:17 +0700

    edit

--- Backend/hashPassword.js
+++
@@ -1,4 +0,0 @@
-// Script nhỏ để hash password (chạy bằng node hashScript.js)
-const bcrypt = require('bcrypt');
-const password = 'admin123'; // Thay bằng password bạn muốn
-bcrypt.hash(password, 10).then(hash => console.log(hash));
