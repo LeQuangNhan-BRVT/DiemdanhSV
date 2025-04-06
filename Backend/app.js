b5904bb711584b1236523209d352ac270a70e5d4
cowship <lequangnhanbrvt58@gmail.com>
2025-04-06 13:47:13 +0700

    deploy

--- Backend/app.js
+++ Backend/app.js
@@ -14,9 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 //Middleware
-app.use(cors({
-  origin: "http://localhost:3000/", 
-}));
+app.use(cors());
 app.use(bodyParser.json());
 
 //routes
